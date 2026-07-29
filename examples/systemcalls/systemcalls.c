0a90060e7bf38604e604010aa68d59d2911c1035
CPM1996 <cpm1996@gmail.com>
2026-07-29 21:12:31 +0200

    Assignment 3 part 1 done

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,5 +1,10 @@
 #include "systemcalls.h"
 
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+
 /**
  * @param cmd the command to execute with system()
  * @return true if the command in @param cmd was executed
@@ -17,7 +22,9 @@ bool do_system(const char *cmd)
  *   or false() if it returned a failure
 */
 
-    return true;
+    int ret = system(cmd);
+
+    return (ret == 0);
 }
 
 /**
@@ -40,28 +47,47 @@ bool do_exec(int count, ...)
     va_start(args, count);
     char * command[count+1];
     int i;
+
+    int childPid;
+    int wStatus;
+    bool result;
+
     for(i=0; i<count; i++)
     {
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
 
-/*
- * TODO:
- *   Execute a system command by calling fork, execv(),
- *   and wait instead of system (see LSP page 161).
- *   Use the command[0] as the full path to the command to execute
- *   (first argument to execv), and use the remaining arguments
- *   as second argument to the execv() command.
- *
-*/
+    childPid = fork();
+    if(childPid < 0)
+    {
+        result = false;
+    }
+
+    if(childPid == 0)
+    {
+        /* Child process */
+        execv(command[0], command);
+
+        /* At this point, execv has failed */
+        exit(1);
+    }
+    else
+    {
+        /* Parent process */
+        if(-1 == waitpid(childPid, &wStatus, 0))
+        {
+            result = false;
+        }
+        else
+        {
+            result = ((WIFEXITED(wStatus)) && (0 == WEXITSTATUS(wStatus))) ? true : false;
+        }
+    }
 
     va_end(args);
 
-    return true;
+    return result;
 }
 
 /**
@@ -75,25 +101,59 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     va_start(args, count);
     char * command[count+1];
     int i;
+
+    int childPid;
+    int wStatus;
+    bool result;
+    int fd;
+
     for(i=0; i<count; i++)
     {
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
 
+    fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+    if(fd < 0)
+    {
+        return false;
+    }
 
-/*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
- *   redirect standard out to a file specified by outputfile.
- *   The rest of the behaviour is same as do_exec()
- *
-*/
+    childPid = fork();
+    if(childPid < 0)
+    {
+        result = false;
+        close(fd);
+    }
+    else if(childPid == 0)
+    {
+        /* Child process */
+        if(dup2(fd, 1) <0)
+        {
+            exit(1);
+        }
+
+        execv(command[0], command);
+
+        /* At this point, execv has failed */
+        exit(1);
+    }
+    else
+    {
+        /* Parent process */
+        close(fd);
+
+        if(-1 == waitpid(childPid, &wStatus, 0))
+        {
+            result = false;
+        }
+        else
+        {
+            result = ((WIFEXITED(wStatus)) && (0 == WEXITSTATUS(wStatus))) ? true : false;
+        }
+    }
 
     va_end(args);
 
-    return true;
+    return result;
 }
